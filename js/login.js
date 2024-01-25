f45120da6e6b32f8740f06f9f593cc4088eb9a86
Z1yoda <ziyoda01092007@gmail.comgit init git config  --global user.email ziyoda01092007@gmil.com>
2024-01-25 19:31:05 +0500

    first commit

--- js/login.js
+++ js/login.js
@@ -4,6 +4,7 @@ const userName = document.querySelector('#userName');
 const password = document.querySelector('#password');
 const button = document.querySelector('#button');
 const formClass = document.querySelector('.form')
+const signUp = document.querySelector('#signUp')
 
 button && button.addEventListener('click', function (e) {
     e.preventDefault();
@@ -25,3 +26,11 @@ button && button.addEventListener('click', function (e) {
         }
     }
 });
+
+signUp && signUp.addEventListener('click', function (e) {
+    e.preventDefault()
+    let currentUrl = window.location.href
+    let pageIndex = currentUrl.search('pages')
+    const domain = currentUrl.substring(0, pageIndex)
+    window.location.assign(`${domain}pages/register.html?`)
+})
